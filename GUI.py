ddc1c4f116090625d1e7fd8d8437065e6185a4c3
Arippz <ahmadarifinmaulidan123@gmail.com>
2026-02-08 00:34:38 +0700

    second setting up

---
+++ GUI.py
@@ -0,0 +1,36 @@
+from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import (
+    QMainWindow,
+    QApplication,
+    QHBoxLayout,
+    QVBoxLayout,
+    QLabel,
+    QLineEdit,
+    QWidget,
+    QPushButton,
+)
+
+import sys
+import os
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.mainContainer = QHBoxLayout()
+
+        self.label1 = QLabel("Welcome text")
+
+        self.mainContainer.addWidget(self.label1)
+        
+        self.widget = QWidget()
+        self.setFixedSize(600, 300)
+        self.setCentralWidget(self.widget)
+        self.widget.setLayout(self.mainContainer)
+
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+app.exec()
