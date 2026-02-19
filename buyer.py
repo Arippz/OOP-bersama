ed9d63351d7f13d8d95e124324b223140c1fc8ed
Arippz <ahmadarifinmaulidan123@gmail.com>
2026-02-19 22:46:57 +0700

    adding email atributtes

--- buyer.py
+++ buyer.py
@@ -1,8 +1,9 @@
 class Customer():
-    def __init__(self, username, transaction, tes, tes2):
+    def __init__(self, username, transaction, email, tes2):
         self.username = username
         self.tes = tes
         self.testi = tes2
+        self.email = email
         # gundar
         # unjani
         # Telkom
